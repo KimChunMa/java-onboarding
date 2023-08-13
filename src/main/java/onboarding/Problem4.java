57cc54de1d4d356cd8fb2ddd501accbbdc48bd50
Chunma <acttesty44@naver.com>
2023-08-13 14:43:03 +0900

    Feat(Problem4): 사전 정보에 대한 문자열 변환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem4 {
+	static Converter converter = new Converter();
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return converter.encode(word);
     }
+
+	public static class Converter{
+		private final int LOWER_OFFSET = 155;
+		private final int UPPER_OFFSET = 219;
+
+		public String encode(String word){
+			String result = "";
+			for(int i = 0 ; i < word.length() ; i++){
+				result += changeChar( word.charAt(i) );
+			}
+			return result;
+		}
+
+		public char changeChar(char letter){
+			if(Character.isUpperCase(letter)){
+				return (char) (LOWER_OFFSET - letter);
+			}else if(Character.isLowerCase(letter)){
+				return (char) (UPPER_OFFSET - letter);
+			}
+			return letter;
+		}
+	}
 }
