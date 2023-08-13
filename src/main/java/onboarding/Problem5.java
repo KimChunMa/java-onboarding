e48f94f3b98eabbe879c855b68ca634bf58216c8
Chunma <acttesty44@naver.com>
2023-08-13 15:20:32 +0900

    Feat(Problem5): 주어진 숫자를 최소한의 금액 화폐로 바꾸는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+	static MoneyChangeHandler moneyChangeHandler = new MoneyChangeHandler();
+
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = Arrays.asList( moneyChangeHandler.converyMoney(money));
+		return answer;
+	}
+
+	public static class MoneyChangeHandler {
+		private final Integer[] currencyArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		private Integer[] result = new Integer[9];
+
+		public Integer[] converyMoney(int money) {
+			for (int i = 0; i < currencyArray.length; i++) {
+				result[i] = money / currencyArray[i];
+				money %= currencyArray[i];
+			}
+			return result;
+		}
+
+	}
 }
