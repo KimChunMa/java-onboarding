4a04cd20495e5f7f47798f14aabceaf3526b42dc
Chunma <acttesty44@naver.com>
2023-08-15 00:29:02 +0900

    Fix(Problem6): 중복된 이메일 리스트 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,23 +1,23 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
-	private final int EMAIL = 1;
-	private final int NICK_NAME = 2;
-	private final int CONSECUTIVE_NICKNAME = 2;
+	private static final int EMAIL = 0;
+	private static final int NICK_NAME = 1;
+	private static final int CONSECUTIVE_NICKNAME = 2;
 
-	ListMapper listMapper = new ListMapper();
+	private static ListMapper listMapper = new ListMapper();
+	private static NicknameValidator nicknameValidator = new NicknameValidator();
 
 	public static List<String> solution(List<List<String>> forms) {
-
-		return null;
+		HashMap<String, String> crewHashMap = listMapper.toHashMap(forms);
+		List<String> duplicateEmailList = nicknameValidator.findDuplicateNicknames(crewHashMap);
+		Collections.sort(duplicateEmailList);
+		return duplicateEmailList;
 	}
 
-	public class ListMapper {
+	public static class ListMapper {
 		public HashMap<String, String> toHashMap(List<List<String>> forms) {
 			HashMap<String, String> crewHashMap = new HashMap<>();
 			for (List<String> list : forms) {
@@ -27,42 +27,54 @@ public class Problem6 {
 			}
 			return crewHashMap;
 		}
-
-		public List<String> subStringNickname(String nickName) {
-			List<String> substringList = new ArrayList<>();
-			for (int i = 0; i < nickName.length() - CONSECUTIVE_NICKNAME; i++) {
-				String substringNickName = nickName.substring(i, CONSECUTIVE_NICKNAME);
-				substringList.add(substringNickName);
-			}
-			return substringList;
-		}
 	}
 
-	public class NicknameValidator {
-		public HashMap<String, String> findDuplicateNicknames(HashMap<String,String> crewHashMap){
-			HashMap<String, String> duplicateNicknames = new HashMap<>();
+	public static class NicknameValidator {
+		static HashMap<String, List<String>> duplicateEmailMap = new HashMap<>();
+		static List<String> duplicateEmail = new ArrayList<>();
 
-			crewHashMap.forEach((email,nickname)->{
-				List<String> nicknameList = listMapper.subStringNickname(nickname);
-				if(findSimilarNickname(nickname , nicknameList)){
-					duplicateNicknames.put(email,nickname);
-				}
+		public List<String> findDuplicateNicknames(HashMap<String, String> crewHashMap) {
+			crewHashMap.forEach((email, nickname) -> {
+				subStringNickname(email, nickname);
 			});
+			findDuplicateEmail();
 
-			return duplicateNicknames;
+			return duplicateEmail;
 		}
 
-		public boolean findSimilarNickname(String nickname , List<String> splitNicknameList){
-			for (String name : splitNicknameList) {
-				if(nickname.indexOf(name)>=0){
-					return true;
-				}
+		public void subStringNickname(String email, String nickName) {
+			for (int i = 0; i <= nickName.length() - CONSECUTIVE_NICKNAME; i++) {
+				String substringNickName = nickName.substring(i, i+CONSECUTIVE_NICKNAME);
+				findKey(substringNickName);
+				putEmail(email, nickName, substringNickName);
 			}
-			return false;
 		}
 
+		public void findKey(String substringNickName){
+			if(!duplicateEmailMap.containsKey(substringNickName)){
+				duplicateEmailMap.put(substringNickName, new ArrayList<>());
+			}
+		}
 
+		public void putEmail(String email, String nickName, String substringNickName) {
+			if(nickName.indexOf(substringNickName)>=0){
+				duplicateEmailMap.get(substringNickName).add(email);
+			}
+		}
+
+		public void findDuplicateEmail(){
+			duplicateEmailMap.forEach((nickname,emailList)->{
+				findListSize(emailList);
+			});
+		}
+
+		public void findListSize(List<String> emailList){
+			if(emailList.size()>=2){
+				duplicateEmail.addAll(emailList);
+			}
+		}
 	}
+
 
 }
 
